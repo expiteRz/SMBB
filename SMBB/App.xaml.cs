f0acd6d34f3e006805de7b5ec4102bed92eaaddc
kazuki-4ys <akki2001.ex@gmail.com>
2021-10-22 21:33:12 +0900

    多重起動できないように変更

--- SMBB/App.xaml.cs
+++ SMBB/App.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,46 @@ namespace SMBB
     /// </summary>
     public partial class App : Application
     {
+        private static readonly Mutex mutex = new Mutex(false, GetMutexString());
+        //SMBBが同じディレクトリで複数起動できないようにディレクトリのハッシュからmutex生成
+        private static bool hasHandle = false;
+        private static string GetMutexString()
+        {
+            byte[] data = Encoding.UTF8.GetBytes(AppDomain.CurrentDomain.BaseDirectory);
+            var md5 = new MD5CryptoServiceProvider();
+            byte[] dest = md5.ComputeHash(data);
+            md5.Clear();
+            var result = new StringBuilder();
+            foreach (byte b in dest)
+            {
+                result.Append(b.ToString("X2"));
+            }
+            return "SMBB" + result.ToString();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Mutexの所有権を取得
+            hasHandle = mutex.WaitOne(0, false);
+            // 取得できなければ多重起動
+            if (!hasHandle)
+            {
+                MessageBox.Show("すでに起動しています", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Shutdown();
+                return;
+            }
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            if (hasHandle) mutex.ReleaseMutex();
+            mutex.Close();
+        }
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            var windows = new MainWindow();
+            windows.Show();
+        }
     }
+
 }
